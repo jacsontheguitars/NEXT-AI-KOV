a57ec19b6537a1273517cdf33370b8cd5cff995b
Jheanpiere Cerdan <jheanpierealejandroupc@gmail.com>
2025-06-04 19:02:25 -0500

    Actualización de dependencias para compatibilidad con Streamlit Cloud

--- setup.py
+++ setup.py
@@ -4,5 +4,17 @@ setup(
     name="next-ai-kov",
     version="0.1",
     packages=find_packages(),
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=[
+        'pandas==1.5.3',
+        'streamlit',
+        'numpy',
+        'pip>=23.1',
+        'setuptools',
+        'wheel',
+        'google-auth-oauthlib==1.1.0',
+        'google-auth-httplib2==0.1.1',
+        'google-api-python-client==2.108.0',
+        'oauth2client==4.1.3'
+    ],
+    python_requires='>=3.10.0',
 )
